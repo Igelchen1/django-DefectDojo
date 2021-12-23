cfc9ef3d416f503652b8e6b7f2e9e4ecf54da5dc
Igelchen1 <maximiliansiegert@hotmail.de>
2021-12-23 13:58:59 +0100

    Wiz CVE Importer and first Unit Test

---
+++ dojo/tools/wiz/parser.py
@@ -0,0 +1,122 @@
+import json
+from urllib.parse import urlparse
+from dojo.models import Endpoint, Finding
+
+
+class wizParser(object):
+    """
+    CVE and Secret scans for Container and VMs
+    """
+
+    def get_scan_types(self):
+        return ["wiz Scan"]
+
+    def get_label_for_scan_types(self, scan_type):
+        return "wiz Scan"
+
+    def get_description_for_scan_types(self, scan_type):
+        return "wiz report file can be imported in JSON format (option --json)."
+
+    def get_findings(self, file, test):
+        
+        try:
+            data = file.read()
+            try:
+                tree = json.loads(str(data, 'utf-8'))
+            except:
+                tree = json.loads(data)
+        except:
+            raise Exception("Invalid format")
+
+
+        items = {}
+        if 'result' in tree:
+            resultTree = tree['result']
+            if 'osPackages' in resultTree:
+                osPackagesTree = resultTree['osPackages']
+
+                for package in osPackagesTree:
+                    for vulnerability in package['vulnerabilities']: 
+                        item = get_item(vulnerability, package["name"], package["version"], test)
+                    unique_key = str(resultTree['__typename'] + str(package['name']  + str(
+                        package['version']) + str(item.severity)))
+                    items[unique_key] = item
+
+                librariesTree = resultTree['libraries']
+                for package in librariesTree:
+                    for vulnerability in package['vulnerabilities']: 
+                        item = get_item(vulnerability, package["name"], package["version"], test)
+                    unique_key = str(resultTree['__typename'] + str(package['name']  + str(
+                        package['version']) + str(item.severity)))
+                    items[unique_key] = item
+            
+        return list(items.values())
+
+
+'''
+def get_items(self, tree, test):
+        items = {}
+        if 'result' in tree:
+            resultTree = tree['result']
+            if 'osPackages' in resultTree:
+                osPackagesTree = resultTree['osPackages']
+
+                for package in osPackagesTree:
+                    for vulnerability in package['vulnerabilities']: 
+                        item = get_item(vulnerability, package["name"], package["version"], test)
+                    unique_key = str(resultTree['scanOriginResource']["name"] + str(package['name']  + str(
+                        package['version']) + str(item.severity)))
+                    items[unique_key] = item
+
+                librariesTree = resultTree['libraries']
+                for package in librariesTree:
+                    for vulnerability in package['vulnerabilities']: 
+                        item = get_item(vulnerability, package["name"], package["version"], test)
+                    unique_key = str(resultTree['scanOriginResource']["name"] + str(package['name']  + str(
+                        package['version']) + str(item.severity)))
+                    items[unique_key] = item
+            
+        return list(items.values())
+'''
+
+
+def get_item(vulnerability, package, version, test):
+
+    fixedVersion = vulnerability['fixedVersion'] if vulnerability['fixedVersion'] else 'No available Fix found.'
+
+    # create the finding object
+    finding = Finding(
+        title=vulnerability['name'] + 'in: ' + package + ' - ' + version,
+        cve=vulnerability['name'],
+        test=test,
+        severity=convert_severity(vulnerability['severity']),
+        description= vulnerability['name'] + 'found in' + package +' on version '+version +'.',
+        mitigation=fixedVersion,
+        references=vulnerability['source'],
+        component_name=package,
+        component_version=version,
+        false_p=False,
+        duplicate=False,
+        out_of_scope=False,
+        mitigated=None,
+        severity_justification="",
+        impact=convert_severity(vulnerability['severity'])
+        )
+
+    return finding 
+ 
+
+def convert_severity(severity):
+    if severity.lower() == 'critical':
+        return "Critical"
+    elif severity.lower() == 'high':
+        return "High"
+    elif severity.lower() == 'medium':
+        return "Medium"
+    elif severity.lower() == 'low':
+        return "Low"
+    elif severity.lower() == 'informational':
+        return "Info"
+    else:
+        return "Info"
+
